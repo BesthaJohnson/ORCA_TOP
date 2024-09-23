0615d95299a8d7960a8f160b335e23e77f5f5638
BesthaJohnson <johnson48001@gmail.com>
2024-09-23 16:39:53 +0530

    Create cbest.sh

---
+++ PHYSICAL_DESIGN/StarRc/inputs1/scripts1/cbest.sh
@@ -0,0 +1,9 @@
+TCAD_GRD_FILE: /home/acs/Desktop/Johnson/ORCA_TOP1/StarRc/inputs/saed32nm_1p9m_Cmax.nxtgrd 
+MAPPING_FILE: /home/acs/Desktop/Johnson/ORCA_TOP1/StarRc/inputs/saed32nm_tf_itf_tluplus.map 
+NETLIST_FORMAT: SPEF
+BLOCK : route_16
+NDM_DATABASE: /home/acs/Desktop/Johnson/ORCA_TOP1/PD/outputs/works/ORCA_TOP.nlib/
+OPERATING_TEMPERATURE: 125
+SELECTED_CORNERS: Cmax
+NETLIST_FILE: /home/acs/Desktop/Johnson/ORCA_TOP1/StarRc/outputs/Cmax_m125_2.spef
+COUPLE_TO_GROUND: NO
