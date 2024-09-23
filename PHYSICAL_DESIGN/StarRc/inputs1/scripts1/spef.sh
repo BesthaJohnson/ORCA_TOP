13ac09f97c242057427c5c02e7c73877876470c8
BesthaJohnson <johnson48001@gmail.com>
2024-09-23 16:43:50 +0530

    Create spef.sh

---
+++ PHYSICAL_DESIGN/StarRc/inputs1/scripts1/spef.sh
@@ -0,0 +1,6 @@
+set p=`pwd`
+mkdir ./out/work_dir/cbest
+cd ./out/work_dir/cbest
+#to generate spef file for the corner m40
+
+/home/tools/synopsys/starrc/N-2017.12-SP3-2/bin/StarXtract -clean /home/acs/Desktop/Johnson/ORCA_TOP1/StarRc/scripts/cbest
